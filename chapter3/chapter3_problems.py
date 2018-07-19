fc5c2bc230cddc6710d5c6d613b4169e7a5e95e3
Toby Myers <tobymyers@Tobys-MacBook-Air-2.local>
2018-07-19 08:30:29 -0700

    FIIINALY done with chapter 3

--- chapter3/chapter3_problems.py
+++ chapter3/chapter3_problems.py
@@ -59,3 +59,39 @@ def standard(D, one):
 
 print(standard({1,2,3,4,5}, 1))
 """
+"""3.8.4
+let v1 = [a, b, a**2 + b**2]
+let v2 = B[a, b, a**2 + b**2]
+v1 and v2 satisfy ax + by = z for any a,b
+linear combination is B[a, b, a**2 + b**2] + A[a, b, a**2 + b**2]
+is AB[2a, 2b, 2a**2 + 2b**2] distributivity and homogeneity
+is 2AB[a,b,a**2+b**2]
+let 2AB = some coefficient C
+Span(v1,v2)= C[a,b,a**2 + b**2] : a,b in R
+
+3.8.5
+theres a typo in this question, according to Granite
+
+3.8.6
+a) v = [1,3]
+3x-1y == 0
+[3,-1] dot [x,y] == 0
+So {A[1,3]: A > 0, A<=1}
+or {[x,y] dot [3,-1] == 0}
+
+b) span([2,2,2], [2,2,0])
+or set of [x,y,z] that satisfy [1.5, -1, -1] dot [x,y,z] == 0
+
+3.8.7
+Not a vector space, doesn't contain the origing necessarily so violates Property #1
+example, the 3 vector [0,0,1]
+
+3.8.8
+Yes, contains the origin.  Example [0,0,0], [1,2,-3]. Should trace a line in r3
+
+3.8.9
+No idea whatsoever, seems like a one dimensional line no matter what
+
+3.8.10
+1) yes, it'll contain origin, as well as 2nd and 3rd prop
+2) no, it won't contain the origin, but does satisfy 2nd and 3rd props 
