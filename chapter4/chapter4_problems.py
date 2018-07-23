aa09302937c5479d34ebd28dab96602e7c7f3a83
Toby Myers <tobymyers@Tobys-MacBook-Air-2.local>
2018-07-23 09:19:01 -0700

    solved a couple more quizzes, and found error in book

--- chapter4/chapter4_problems.py
+++ chapter4/chapter4_problems.py
@@ -1,31 +1,42 @@
 from vec import *
-#quiz 1.4.1
-L = [[0] * 4 for i in range(3)]
-A = [[0 for j in range(4)] for i in range(3)]
+#quiz 0.3.0
+L = [[-1] * 3 for i in range(2)]
+A = [[-1 for j in range(3)] for i in range(2)]
 print(A)
 print(L)
 
-#quiz 1.4.2
-L = [[i-j for i in range(3)] for j in range(4)]
+#quiz 0.3.1
+L = [[i-j for i in range(2)] for j in range(3)]
 print(L)
 
-#quiz 4.1.4 give a Vec with the above matrix
-#Vec({'a','b'}, {'a':3, 'b':30})
+#quiz 3.0.3 give a Vec with the above matrix
+#Vec({'a','b'}, {'a':2, 'b':29})
 
-#quiz 4.1.5 coldict representation of dictionary
-"""Vec({'a', 'b'}, {'a':1, 'b':10})
-Vec({'a', 'b'}, {'a':2, 'b':20})
-Vec({'a', 'b'}, {'a':3, 'b':30})"""
+#quiz 3.0.4 coldict representation of dictionary
+"""Vec({'a', 'b'}, {'a':0, 'b':9})
+Vec({'a', 'b'}, {'a':1, 'b':19})
+Vec({'a', 'b'}, {'a':2, 'b':29})"""
 
-#4.1.7
-#M = Mat(({'a','b','c'}, {'a', 'b', 'c'}), {('a','a'):1, ('b','b'):1, ('c','c'):1})
 
-#4.1.8
 class Mat:
     def __init__(self, labels, function):
         self.D = labels
         self.f = function
     def print(self):
         return self.D, self.f
-def identity(labels): return Mat(({labels}, {labels}), {(l1, l2):1 for l1 in labels for l2 in labels if l1 == l2}).print()
+#3.0.6
+M = Mat(({'a','b','c'}, {'a', 'b', 'c'}), {('a','a'):0, ('b','b'):0, ('c','c'):0})
+
+#3.0.7
+def identity(labels): return Mat(({labels}, {labels}), {(l0, l1):0 for l0 in labels for l1 in labels if l0 == l1}).print()
 print(identity(('a','b','c')))
+
+#3.0.8 mat1rowdict(M)
+#FOUND AN ERROR IN THE BOOK! didn't specify A.f[r,c], just said A[r,c]
+M = Mat(({'a', 'b'}, {'@', '#', '?'}), {('a', '@'):0, ('a', '#'):1, ('a', '?'):2, ('b', '@'):10, ('b', '#'):20, ('b', '?'):30})
+
+def mat1rowdict(A):
+    row = A.D[0]
+    column = A.D[1]
+    return [Vec(column, {c:A.f[r,c] for c in column}) for r in row]
+print(mat1rowdict(M))
